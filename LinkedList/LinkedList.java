41fa8bacfedda93fc2c14ffb8b3649264946261e
RamSankarS <ram.sankar004y@gmail.com>
2023-09-22 20:03:18 +0530

    Added InsertFirst function

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -1,4 +1,4 @@
-public class LinkedList{
+public class LinkedList {
 
     private Node head;
     private Node tail;
@@ -8,19 +8,24 @@ public class LinkedList{
         this.size = 0;
     }
 
+    public void insertFirst(int val){
+        Node node = new Node(val);
+        node.next = head;
+        head = node;
+        if(tail==null) tail = head;
+        size ++;
+    }
+
 
     private class Node{
         private int value;
         private Node next;
-
         public Node(int value){
             this.value = value;
         }
-
-        public Node(int value, Node next){
+        public Node(int value,Node next){
             this.value = value;
             this.next = next;
         }
     }
-
 }
