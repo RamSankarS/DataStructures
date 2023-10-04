3d54ea960c7e8fc2a40ad7d57c3ec210b6c480b2
RamSankarS <ram.sankar004y@gmail.com>
2023-10-04 20:18:31 +0530

    Added Infinite Sorted Array question

--- BinarySearch/SearchInfiniteSortedArray.java
+++ BinarySearch/SearchInfiniteSortedArray.java
@@ -1,6 +1,19 @@
 public class SearchInfiniteSortedArray {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+        int target = 12;
+        System.out.println(result(arr,target));
 
+    }
+    static int result(int[] arr,int target){
+        int start = 0;
+        int end = 1;
+        while(arr[end] < target){
+            int newStart = end + 1;
+            end = end + (end - start + 1)*2;
+            start = newStart;
+        }
+        return BinarySearchInfinity(arr,target,start,end);
     }
     static int BinarySearchInfinity(int[] arr, int target, int start,int end) {
         int mid;
